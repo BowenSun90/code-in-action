3ad1b338052d871827791a693d32fcc67b25dc70
sunbw <bowen.sun@tendcloud.com>
2018-08-19 18:01:17 +0800

    add flink word count example program

---
+++ flink-in-action/streaming-wordcount/src/main/scala/com/alex/space/SocketWindowWordCount.scala
@@ -0,0 +1,57 @@
+package com.alex.space
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
+/**
+  * @author Alex
+  *         Created by Alex on 2018/8/19.
+  */
+object SocketWindowWordCount {
+
+  def main(args: Array[String]): Unit = {
+    // the host and the port to connect to
+    var hostname: String = "localhost"
+    var port: Int = 9000
+
+    try {
+      val params = ParameterTool.fromArgs(args)
+      hostname = if (params.has("hostname")) params.get("hostname") else "localhost"
+      port = params.getInt("port")
+    } catch {
+      case e: Exception => {
+        System.err.println("No port specified. Please run 'SocketWindowWordCount " +
+          "--hostname <hostname> --port <port>', where hostname (localhost by default) and port " +
+          "is the address of the text server")
+        System.err.println("To start a simple text server, run 'netcat -l <port>' " +
+          "and type the input text into the command line")
+        e.printStackTrace()
+      }
+    }
+
+    // get the execution environment
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // get input data by connecting to the socket
+    val text = env.socketTextStream(hostname, port, '\n')
+
+    // parse the data, group it, window it, and aggregate the counts
+    val windowCounts = text
+      .flatMap { w => w.split("\\s") }
+      .map { w => WordWithCount(w, 1) }
+      .keyBy("word")
+      .timeWindow(Time.seconds(5), Time.seconds(1))
+      .sum("count")
+
+
+    // print the results with a single thread, rather than in parallel
+    windowCounts.print().setParallelism(1)
+
+    env.execute("Socket Window WordCount")
+
+  }
+
+  case class WordWithCount(word: String, count: Long)
+
+}
