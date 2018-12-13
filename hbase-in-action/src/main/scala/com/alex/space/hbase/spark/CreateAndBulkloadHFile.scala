c744f5d391b92cff3c6bed770ec7fffefebc20e0
sunbw <bowen.sun@tendcloud.com>
2018-12-13 17:37:59 +0800

    update hbase bulk load demo

--- hbase-in-action/src/main/scala/com/alex/space/hbase/spark/CreateAndBulkloadHFile.scala
+++ hbase-in-action/src/main/scala/com/alex/space/hbase/spark/CreateAndBulkloadHFile.scala
@@ -2,6 +2,7 @@ package com.alex.space.hbase.spark
 
 import java.util.Date
 
+import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.hbase.client.{ConnectionFactory, HTable, Table}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
@@ -12,7 +13,8 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
-  * Spark创建并
+  * Spark创建HFile
+  * Bulkload导入HBase table
   *
   * @author Alex
   *         Created by Alex on 2018/10/26.
@@ -20,26 +22,31 @@ import org.apache.spark.{SparkConf, SparkContext}
 object CreateAndBulkloadHFile {
 
   def main(args: Array[String]): Unit = {
-    // 生成HFile
+    // Step1 生成HFile
+    // HFile要生成在HDFS上
+    // 本地调试，HBase Bulkload会失败
     val sc = new SparkContext(new SparkConf().setMaster("local[*]").setAppName("DumpFile"))
-    val cf = "d"
-    val conf = HBaseConfiguration.create()
 
+    val hadoopConf = new Configuration()
+
+    val tableName = "bulk_load_table_1"
+    val cf = "d"
     val date = new Date().getTime
 
     val srcRdd = sc.parallelize(
       Array(
         // rowkey,family,qualifier,value
-        (Bytes.toBytes("1"), Bytes.toBytes(cf), Bytes.toBytes("c1"), Bytes.toBytes("v1")),
-        (Bytes.toBytes("1"), Bytes.toBytes(cf), Bytes.toBytes("c2"), Bytes.toBytes("v2")),
-        (Bytes.toBytes("2"), Bytes.toBytes(cf), Bytes.toBytes("c1"), Bytes.toBytes("v3")),
-        (Bytes.toBytes("2"), Bytes.toBytes(cf), Bytes.toBytes("c2"), Bytes.toBytes("v4"))
+        (Bytes.toBytes("rowkey1"), Bytes.toBytes(cf), Bytes.toBytes("c1"), Bytes.toBytes("v1")),
+        (Bytes.toBytes("rowkey2"), Bytes.toBytes(cf), Bytes.toBytes("c2"), Bytes.toBytes("v2")),
+        (Bytes.toBytes("rowkey1"), Bytes.toBytes(cf), Bytes.toBytes("c1"), Bytes.toBytes("v3")),
+        (Bytes.toBytes("rowkey3"), Bytes.toBytes(cf), Bytes.toBytes("c2"), Bytes.toBytes("v4"))
       ))
 
-    val rdd = srcRdd.map(x => {
-      // 转换成HFile需要的格式
-      (new ImmutableBytesWritable(x._1), new KeyValue(x._1, x._2, x._3, date, x._4))
-    })
+    val rdd = srcRdd
+      .map(x => {
+        // 转换成HFile需要的格式
+        (new ImmutableBytesWritable(x._1), new KeyValue(x._1, x._2, x._3, date, x._4))
+      })
 
     // 生成的HFile的临时保存路径
     val stagingFolder = "data/hbase/hfile/"
@@ -47,26 +54,37 @@ object CreateAndBulkloadHFile {
       classOf[ImmutableBytesWritable],
       classOf[KeyValue],
       classOf[HFileOutputFormat2],
-      conf)
+      hadoopConf)
 
-    // HFile导入到Hbase
-    val load = new LoadIncrementalHFiles(conf)
-    val tableName = "test_blukload"
+    // Step2 HFile导入到Hbase
+    //  hbase conf
+    val hbaseConf = HBaseConfiguration.create()
+    // dev：环境变量设置HADOOP_USER_NAME
+    hbaseConf.set("HADOOP_USER_NAME", "hadoop")
+    hbaseConf.set("hbase.zookeeper.quorum", "172.23.4.138,172.23.4.139,172.23.4.140")
+    hbaseConf.set("hbase.zookeeper.property.clientPort", "2181")
+    hbaseConf.set("hbase.rootdir", "hdfs://namenode01.td.com/hbase")
+    hbaseConf.set("fs.defaultFS", "hdfs://namenode01.td.com")
+    hbaseConf.set("hbase.scanner.timeout.period", "60000")
+    val load = new LoadIncrementalHFiles(hbaseConf)
 
-    val conn = ConnectionFactory.createConnection(conf)
+    val conn = ConnectionFactory.createConnection(hbaseConf)
     val table: Table = conn.getTable(TableName.valueOf(tableName))
-    try {
 
+    try {
       val regionLocator = conn.getRegionLocator(TableName.valueOf(tableName))
-      val job = Job.getInstance(conf)
-      job.setJobName("DumpFile")
-      //此处最重要,需要设置文件输出的key,因为我们要生成HFil,所以outkey要用ImmutableBytesWritable
+      val job = Job.getInstance(hbaseConf)
+      job.setJobName("LoadFile")
+      // 此处最重要，需要设置文件输出的key，要生成HFile，所以OutputKey要用ImmutableBytesWritable
       job.setMapOutputKeyClass(classOf[ImmutableBytesWritable])
       job.setMapOutputValueClass(classOf[KeyValue])
 
-      //配置HFileOutputFormat2的信息
+      // 配置HFileOutputFormat2的信息
       HFileOutputFormat2.configureIncrementalLoad(job, table, regionLocator)
-      load.doBulkLoad(new Path(stagingFolder), table.asInstanceOf[HTable])
+
+      // bulkload后会删除hfile，要做好备份
+      val hdfsPath = "/bulkload/test1"
+      load.doBulkLoad(new Path(hdfsPath), table.asInstanceOf[HTable])
 
     } finally {
       table.close()
@@ -74,4 +92,5 @@ object CreateAndBulkloadHFile {
     }
 
   }
+
 }
